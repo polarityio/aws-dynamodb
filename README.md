717d0dd676a29d5ae4e62a4934d82c3af9b05e36
Ed <ed@polarity.io>
2022-04-11 23:00:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,154 @@
+# Polarity DynamoDB Integration
+
+The Polarity DynamoDB integration enables querying of your DynamoDB tables hosted in AWS.
+
+## DynamoDB
+
+### AWS Region
+
+The AWS Region for your DynamoDB database.  The DynamoDB endpoint is based on your selected region.  The integration does not support local developer instances of DynamoDB.
+
+### AWS Access Key Id
+
+The access key ID that identifies the security credentials used to authenticate to the specified DynamoDB instance.  As this integration is designed only for `SELECT` queries we recommend using security credentials tied to an IAM user with only the `dynamodb:PartiQLSelect` action allowed on your DynamoDB table. 
+
+For more information about IAM Security Policies with PartiQL for DynamoDB please see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/ql-iam.html
+
+### AWS Secret Access Key
+
+The secret access key associated with your Access Key Id that can be used to sign requests.
+
+### PartiQL Query
+
+The PartiQL query to run against your DynamoDB instance.  DynamoDB does not support all PartiQL operations and functions.  Additionally,
+if you do not include a partition key field in your `WHERE` clause using the equality `=` operator or the `IN` operator the query will perform a full
+table scan.
+
+For more information on how to use PartiQL with DynamoDB please see: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/ql-reference.html
+
+For specific information on writing `SELECT` queries please see: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/ql-reference.select.html
+
+### Summary Attributes
+
+Comma delimited list of attribute names to include as part of the summary.  JSON dot notation can be used to target nested attributes. Attributes must be returned by your PartiQL query.  You can add a label to your summary attribute by prepending the label to the attribute path and separating it with a colon (i.e., "<label>:<json path>").
+
+### Detail Attributes
+
+Comma delimited list of attribute names to include as part of the details block.   JSON dot notation can be used to target nested attributes. Attributes must be returned by your PartiQL query.  You can add a label to your detail attribute by prepending the label to the attribute path and separating it with a colon (i.e., "<label>:<json path>"). If left blank, all fields will be displayed as a JSON document.
+
+## Specifying Attributes
+
+Attributes to display can be specified via the Summary Attributes and Detail Attributes options.  Both options support the same syntax which is a comma delimited list of attribute specifications.  An attribute specification at a minimum is the name of the attribute.  Nested attributes can be targeted using JSON dot notation. In addition, a `type` can be specified for the attribute.  The `type` currently supports various date formats since DynamoDB does not have a native date type.
+
+Attributes can be specified with the following three forms:
+
+```
+<attribute>
+<label>:<attribute>
+<label>:<type>:<attribute>
+```
+
+As an example, suppose we have the following document returned by our PartiQL query:
+
+```
+{
+  "ip": "8.8.8.8",
+  "software": {
+    "antivirus": "Microsoft Defender"
+  },
+  "ports": [80, 443],
+  "created": 1649724681
+}
+```
+
+We want our summary tag to display the antivirus software used with the label "AV".  In this example the antivirus attribute is nested under the software attribute.  We can do that with the following:
+
+```
+AV:software.av
+```
+
+This will display the summary tag `AV: Microsoft Defender`.  If we also want to include the IP Address in the summary tags we can do the following:
+
+```
+ip, AV:software.av
+```
+
+Which will display the summary tags `8.8.8.8` and `AV: Microsoft Defender`.
+
+If we want to display the `created` attribute and specify that it is a Unix epoch time date in seconds we can do the following:
+
+```
+Created:date-seconds:created
+```
+
+If you are specifying a `type` for the attribute then you must also always specify a `label`.  The integration supports the following date types:
+
+* date-iso
+
+```
+2016
+2016-05
+201605
+2016-05-25
+20160525
+2016-05-25T09
+2016-05-25T09:24
+2016-05-25T09:24:15
+2016-05-25T09:24:15.123
+2016-05-25T0924
+2016-05-25T092415
+2016-05-25T092415.123
+2016-05-25T09:24:15,123
+2016-W21-3
+2016W213
+2016-W21-3T09:24:15.123
+2016W213T09:24:15.123
+2016-200
+2016200
+2016-200T09:24:15.123
+09:24
+09:24:15
+09:24:15.123
+09:24:15,123
+```  
+* date-http
+  
+```
+Sunday, 06-Nov-94 08:49:37 GMT
+Sun, 06 Nov 1994 08:49:37 GMT
+```
+
+* date-rfc2822
+  
+```
+Tue, 01 Nov 2016 13:23:12 +0630
+```
+* date-sql
+  
+```
+2017-05-15
+2017-05-15 09:24:15
+09:24:15
+```
+
+* date-seconds
+  
+```
+1542674993
+```
+* date-millis  
+
+```
+1542674993410
+```
+
+
+## Installation Instructions
+
+Installation instructions for integrations are provided on the [PolarityIO GitHub Page](https://polarityio.github.io/).
+
+## Polarity
+
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision making.  For more information about the Polarity platform please see:
+
+https://polarity.io/
---
+++ integration.js
@@ -0,0 +1,269 @@
+'use strict';
+
+const async = require('async');
+const { DynamoDBClient, ExecuteStatementCommand } = require('@aws-sdk/client-dynamodb');
+const { unmarshall } = require('@aws-sdk/util-dynamodb');
+const get = require('lodash.get');
+const { DateTime } = require('luxon');
+
+const entityTemplateReplacementRegex = /{{entity}}/g;
+
+let Logger;
+let originalOptions = {};
+let dbClient = null;
+
+function startup(logger) {
+  Logger = logger;
+}
+
+/**
+ * Used to escape single quotes in entities and remove any newlines
+ * @param entityValue
+ * @returns {*}
+ */
+function escapeEntityValue(entityValue) {
+  const escapedValue = entityValue
+    .replace(/(\r\n|\n|\r)/gm, '')
+    .replace(/\\/, '\\\\')
+    .replace(/'/g, '\\');
+  Logger.trace({ entityValue, escapedValue }, 'Escaped Entity Value');
+  return escapedValue;
+}
+
+function createQuery(entity, options) {
+  return {
+    Statement: options.query,
+    Parameters: [
+      {
+        S: options.queryParameter.replace(entityTemplateReplacementRegex, escapeEntityValue(entity.value))
+      }
+    ],
+    Limit: options.limit
+  };
+}
+
+function optionsHaveChanged(options) {
+  if (
+    originalOptions.region !== options.region ||
+    originalOptions.endpoint !== options.endpoint ||
+    originalOptions.accessKeyId !== options.accessKeyId ||
+    originalOptions.secretAccessKey !== options.secretAccessKey
+  ) {
+    originalOptions = options;
+    return true;
+  }
+  return false;
+}
+
+function summaryTagsOptionHasChanged(options) {
+  if (originalOptions.summaryTags !== options.summaryTags) {
+    originalOptions = options;
+    return true;
+  }
+
+  return false;
+}
+
+function errorToPojo(err, detail) {
+  return err instanceof Error
+    ? {
+        ...err,
+        name: err.name,
+        message: err.message,
+        stack: err.stack,
+        detail: detail ? detail : err.detail ? err.detail : 'Unexpected error encountered'
+      }
+    : err;
+}
+
+function parseAttribute(attribute, parser) {
+  switch (parser) {
+    case null:
+      return attribute;
+      break;
+    case 'date-iso':
+      return DateTime.fromISO(attribute).toLocaleString(DateTime.DATETIME_SHORT);
+      break;
+    case 'date-http':
+      return DateTime.fromHTTP(attribute).toLocaleString(DateTime.DATETIME_SHORT);
+      break;
+    case 'date-rfc2822':
+      return DateTime.fromRFC2822(attribute).toLocaleString(DateTime.DATETIME_SHORT);
+      break;
+    case 'date-sql':
+      return DateTime.fromSQL(attribute).toLocaleString(DateTime.DATETIME_SHORT);
+      break;
+    case 'date-seconds':
+      return DateTime.fromSeconds(+attribute).toLocaleString(DateTime.DATETIME_SHORT);
+      break;
+    case 'date-millis':
+      return DateTime.fromSeconds(+attribute).toLocaleString(DateTime.DATETIME_SHORT);
+      break;
+    default:
+      return attribute;
+  }
+}
+
+/**
+ * Takes the attribute option string which is a comma delimited list of attributes to display and converts
+ * it into an object of the format:
+ * ```
+ * {
+ *     label: <attribute label>,
+ *     attribute: <attribute name>,
+ *     parser: <attribute parser>
+ * }
+ * ```
+ * The label is optional and is the display label to be used for the attribute.  If no label is provided then the
+ * attribute name is used
+ *
+ * The attribute is the "name" of the attribute
+ *
+ * The parser is the parser to use if specified for converting the attribute value (currently supports various
+ * date parsers since DynamoDB does not have date support).
+ * then
+ * @param attributeOption
+ * @returns {*}
+ */
+function processAttributeOption(attributeOption) {
+  const fields = attributeOption.split(',').map((column) => {
+    const tokens = column.split(':');
+    if (tokens.length === 1) {
+      return {
+        label: tokens[0].trim(),
+        attribute: tokens[0].trim(),
+        parser: null
+      };
+    } else if (tokens.length === 2) {
+      return {
+        label: tokens[0].trim(),
+        attribute: tokens[1].trim(),
+        parser: null
+      };
+    } else if (tokens.length === 3) {
+      return {
+        label: tokens[0].trim(),
+        attribute: tokens[2].trim(),
+        parser: tokens[1].trim().toLowerCase()
+      };
+    }
+  });
+
+  return fields;
+}
+
+function getDetails(results, options) {
+  // If not detail attributes are specified then we just display the
+  // whatever DynamoDB returns using the JSON viewer
+  if (options.detailAttributes.trim().length === 0) {
+    return {
+      showAsJson: true,
+      results
+    };
+  }
+
+  const details = [];
+  const detailAttributes = processAttributeOption(options.detailAttributes);
+
+  results.forEach((result) => {
+    const document = [];
+    detailAttributes.forEach((attributeObj) => {
+      const attributeValue = get(result, attributeObj.attribute);
+      if (attributeValue) {
+        document.push({
+          key: attributeObj.label,
+          value: parseAttribute(attributeValue, attributeObj.parser)
+        });
+      }
+    });
+
+    if (document.length > 0) {
+      details.push(document);
+    }
+  });
+
+  return {
+    showAsJson: false,
+    results: details
+  };
+}
+
+function getSummaryTags(results, options) {
+  const tags = [];
+  const summaryAttributes = processAttributeOption(options.summaryAttributes);
+
+  summaryAttributes.forEach((attributeObj) => {
+    results.forEach((result) => {
+      const tag = get(result, attributeObj.attribute);
+      if (tag) {
+        if (attributeObj.label) {
+          tags.push(`${attributeObj.label}: ${parseAttribute(tag, attributeObj.parser)}`);
+        } else {
+          tags.push(parseAttribute(tag, attributeObj.parser))
+        }
+      }
+    });
+  });
+
+  if (tags.length === 0) {
+    tags.push(`${result.length} ${result.length === 1 ? 'result' : 'results'}`);
+  }
+  return tags;
+}
+
+async function doLookup(entities, options, cb) {
+  let lookupResults;
+
+  if (optionsHaveChanged(options) || dbClient === null) {
+    const clientOptions = {
+      region: options.region.value,
+      credentials: {
+        accessKeyId: options.accessKeyId,
+        secretAccessKey: options.secretAccessKey
+      }
+    };
+    Logger.trace({ clientOptions }, 'Creating new DynamoDB client');
+    dbClient = new DynamoDBClient(clientOptions);
+  }
+
+  const searchTasks = entities.map((entity) => {
+    return async () => {
+      const query = new ExecuteStatementCommand(createQuery(entity, options));
+      Logger.trace({ query }, 'search partiQL query');
+      const result = await dbClient.send(query);
+      if (Array.isArray(result.Items) && result.Items.length === 0) {
+        return {
+          entity,
+          data: null
+        };
+      } else {
+        // Convert from the dynamodb document to regular javascript object
+        // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/modules/_aws_sdk_util_dynamodb.html#unmarshall-1
+        const resultAsJson = result.Items.map(unmarshall);
+        return {
+          entity,
+          data: {
+            summary: getSummaryTags(resultAsJson, options),
+            details: getDetails(resultAsJson, options)
+          }
+        };
+      }
+    };
+  });
+
+  try {
+    lookupResults = await async.parallelLimit(searchTasks, 10);
+  } catch (lookupError) {
+    Logger.error(lookupError, 'doLookup error');
+    return cb(errorToPojo(lookupError, 'Error running PartiQL query'));
+  }
+
+  Logger.trace({ lookupResults }, 'lookup results');
+
+  cb(null, lookupResults);
+}
+
+module.exports = {
+  doLookup,
+  startup
+};
